447cda4c3e431d6d3d31a86e8d185eb860baca07
clearink <1561939638@qq.com>
2021-04-21 07:36:26 +0800

    update: constant to map

--- src/utils/ConstantToMap.ts
+++ src/utils/ConstantToMap.ts
@@ -1,73 +1,94 @@
-type ExtendFunction<Key = number, Value = string, Return = any> = (value: Map<Key, Value>, list: Array<[Key, Value]>) => Return
+interface ExtendFunctionArg<Key = number, Value = string, KM = string> {
+	map: Map<Key, Value>;
+	list: Array<[Key, Value]>;
+	match: ConstantToMapClass<Key, Value, KM>["match"];
+	keyMap: Map<KM, Value>;
+	keyMatch: ConstantToMapClass<Key, Value, KM>["keyMatch"];
+}
+type ExtendFunction<Key = number, Value = string, KM = string, Return = any> = (
+	args: ExtendFunctionArg<Key, Value, KM>
+) => Return;
 
-class ConstantToMapClass<K = number, V = string> {
+class ConstantToMapClass<K = number, V = string, KM = string> {
+	constructor(public map: Map<K, V>, public keyMap: Map<KM, V>) {}
 
-    constructor(public map: Map<K, V>) {
-    }
+	// 属性扩展
+	public extend<Return = any>(fn: ExtendFunction<K, V, KM, Return>) {
+		return fn({
+			map: this.map,
+			list: Array.from(this.map),
+			match: this.match,
+			keyMap: this.keyMap,
+			keyMatch: this.keyMatch,
+		});
+	}
 
-    // 属性扩展
-    public extend<Return = any>(fn: ExtendFunction<K, V, Return>) {
-        return fn(this.map, Array.from(this.map));
-    }
-
-    public match<Key extends K>(key: Key) {
-        // 匹配 属性
-        return this.map.get(key);
-    }
-
-    public concat<Key extends K, Value extends V>(key: Key, value: Value) {
-        this.map.set(key, value);
-        return this;
-    }
-
-
-
-
+	public match<Key extends K>(key: Key) {
+		// 匹配 属性
+		return this.map.get(key);
+	}
+	public keyMatch<Key extends KM>(key: Key) {
+		// 匹配 属性
+		return this.keyMap.get(key);
+	}
 }
 
 /** 转换成对象数组 */
 export function ConvertObjectArray(list: Array<any[]>, ...nameList: string[]) {
-    if (list.length !== 0 && nameList.length !== list[0].length) {
-        console.error(`无法正确转换成对象数组,请检查参数:\n ${list}`);
-    }
-    return list.map((itemArr) => itemArr.reduce((pre, cur, index) =>
-        ({ ...pre, [nameList[index]]: cur })
-        , {}),
-    );
+	if (list.length !== 0 && nameList.length !== list[0].length) {
+		console.error(`无法正确转换成对象数组,请检查参数:\n ${list}`);
+	}
+	return list.map((itemArr) => itemArr.reduce((pre, cur, index) => ({ ...pre, [nameList[index]]: cur }), {}));
 }
-
 
 /** 根据参数转换成map 获得正确的数据顺序与类型 */
 
-export default function ConstantToMap<Key = number, Value = string>(...constantList: [Key, Value]) {
-    if (constantList.length % 2) {
-        console.error(`键值对无法匹配,请检查参数\n ${constantList}`);
-    }
+export default function ConstantToMap<Key = number, Value = string, KM = string>(
+	...constantList: Array<Key | Partial<Value>>
+) {
+	if (constantList.length % 2) {
+		console.error(`键值对无法匹配,请检查参数\n ${constantList}`);
+	}
 
-    // const map = new Map<Key, TMapValue<typeof constantList[1]>>();
-    // for (let i = 0; i < constantList.length - 1; i += 2) {
-    //     const key = constantList[i] as any;
-    //     const value = constantList[i + 1] as any;
-    //     map.set(key, value);
-    // }
-    return new ConstantToMapClass(new Map());
+	const map = new Map<Key, Value>();
+	const keyMap = new Map<KM, Value>();
+	for (let i = 0; i < constantList.length - 1; i += 2) {
+		const key = constantList[i] as any;
+		const value = constantList[i + 1] as any;
+		map.set(key, value);
+
+		if (value.key) keyMap.set(value.key, value);
+	}
+	return new ConstantToMapClass(map, keyMap);
 }
-function ACTM<Key = number, Value = string>(key: Key, value: Value) {
-    const map = new Map<Key, Partial<Value>>();
-    map.set(key, value);
-    return new ConstantToMapClass(map);
+
+export interface IValue {
+	color: string;
+	text: string;
+	key: "in" | "out";
 }
-const a = ACTM(0, { text: '服务内', color: 'green', key: 'in' })
-    .concat(1, { text: '服务内', color: 'green', key: 'in' })
-    .concat(2, { text: '服务内', color: 'green', key: 'in' })
-    .concat(3, { text: '服务内', color: 'green' });
+const a = ConstantToMap<number, IValue, IValue["key"]>(
+	0,
+	{ text: "服务内" },
+	1,
+	{ text: "服务内1", color: "green1", key: "in" },
+	2,
+	{ text: "服务内2", color: "green2", key: "in" },
+	3,
+	{ text: "服务内3", color: "green3", key: 'in' },
+    4,{}
+).extend((args) => ({ ...args, default: -1 }));
+
 const status = 1;
-const d = a.match(0)?.text;
-console.log(a)
+console.log(a.keyMatch("in"));
 
 /**
  * 需求
  * 1. 根据 status 映射 text done
  * 2. 根据 key 匹配 status pending
  * 3. 转换成list
+ */
+/**
+ * 目标
+ *
  */

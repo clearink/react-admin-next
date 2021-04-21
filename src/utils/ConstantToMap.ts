d9d54bdd99f45427fec9e3b83f72887d805fe047
clearink <1561939638@qq.com>
2021-04-21 18:07:44 +0800

    update: 优化ConstantToMap

--- src/utils/ConstantToMap.ts
+++ src/utils/ConstantToMap.ts
@@ -1,94 +1,108 @@
-interface ExtendFunctionArg<Key = number, Value = string, KM = string> {
-	map: Map<Key, Value>;
-	list: Array<[Key, Value]>;
-	match: ConstantToMapClass<Key, Value, KM>["match"];
-	keyMap: Map<KM, Value>;
-	keyMatch: ConstantToMapClass<Key, Value, KM>["keyMatch"];
+interface ConstantItem {
+	key?: string;
+	status: any;
+	[k: string]: any;
 }
-type ExtendFunction<Key = number, Value = string, KM = string, Return = any> = (
-	args: ExtendFunctionArg<Key, Value, KM>
-) => Return;
+// 可选中排除一些必选项
+type PartialExcludeKey<T, K extends keyof T> = Partial<T> & { [P in K]-?: T[P] };
 
-class ConstantToMapClass<K = number, V = string, KM = string> {
-	constructor(public map: Map<K, V>, public keyMap: Map<KM, V>) {}
+type FunctionToArray<K, V, R = any> = (value: [K, V], index: number, array: [K, V][]) => Partial<R>;
+class ConstantToMapClass<V extends ConstantItem> {
+	constructor(public mapStatus: Map<V["status"], V>, public mapKey: Map<V["key"], V>) {}
+
+	get keys() {
+		return Array.from(this.mapStatus.keys());
+	}
+
+	get values() {
+		return Array.from(this.mapStatus.values());
+	}
+
+	// map添加一个
+	push(value: PartialExcludeKey<V, "status">) {
+		this.mapStatus.set(value["status"], value as V);
+		if (value.key) this.mapKey.set(value["key"], value as V);
+		return this;
+	}
 
 	// 属性扩展
-	public extend<Return = any>(fn: ExtendFunction<K, V, KM, Return>) {
-		return fn({
-			map: this.map,
-			list: Array.from(this.map),
-			match: this.match,
-			keyMap: this.keyMap,
-			keyMatch: this.keyMatch,
-		});
+	public extend<T = object>(value: T) {
+		return Object.assign(this, value);
 	}
 
-	public match<Key extends K>(key: Key) {
+	// 以 status 去 匹配 value
+	public matchStatus<Key extends V["status"]>(key: Key) {
 		// 匹配 属性
-		return this.map.get(key);
+		return this.mapStatus.get(key);
 	}
-	public keyMatch<Key extends KM>(key: Key) {
-		// 匹配 属性
-		return this.keyMap.get(key);
-	}
-}
 
-/** 转换成对象数组 */
-export function ConvertObjectArray(list: Array<any[]>, ...nameList: string[]) {
-	if (list.length !== 0 && nameList.length !== list[0].length) {
-		console.error(`无法正确转换成对象数组,请检查参数:\n ${list}`);
+	// 以 key 去匹配 value + status
+	public matchKey<Key extends V["key"]>(key: Key) {
+		// 匹配 属性
+		return this.mapKey.get(key);
 	}
-	return list.map((itemArr) => itemArr.reduce((pre, cur, index) => ({ ...pre, [nameList[index]]: cur }), {}));
+
+	// 转换成数组
+	public toArray<R>(fn: FunctionToArray<V["status"], V, R>) {
+		const list = Array.from(this.mapStatus).map(fn);
+		return Object.assign(this, { list });
+	}
 }
 
 /** 根据参数转换成map 获得正确的数据顺序与类型 */
 
-export default function ConstantToMap<Key = number, Value = string, KM = string>(
-	...constantList: Array<Key | Partial<Value>>
+export default function ConstantToMap<Value extends ConstantItem>(
+	// 其他的可以省略 但是 status 必须要有
+	...list: PartialExcludeKey<Value, "status">[]
 ) {
-	if (constantList.length % 2) {
-		console.error(`键值对无法匹配,请检查参数\n ${constantList}`);
-	}
+	const mapStatus = new Map<Value["status"], Value>();
+	const mapKey = new Map<Value["key"], Value>();
 
-	const map = new Map<Key, Value>();
-	const keyMap = new Map<KM, Value>();
-	for (let i = 0; i < constantList.length - 1; i += 2) {
-		const key = constantList[i] as any;
-		const value = constantList[i + 1] as any;
-		map.set(key, value);
-
-		if (value.key) keyMap.set(value.key, value);
+	for (let i = 0; i < list.length; i++) {
+		const item = list[i] as Value;
+		if (item.status) mapStatus.set(item.status, item);
+		if (item.key) mapKey.set(item.key, item);
 	}
-	return new ConstantToMapClass(map, keyMap);
+	return new ConstantToMapClass(mapStatus, mapKey);
 }
-
-export interface IValue {
-	color: string;
+// 根据业务的不同定义不同的 type
+interface AssetConstType {
 	text: string;
-	key: "in" | "out";
+	status: 0 | 1 | 2 | 3;
+	key: "server" | "terminal" | "unknown";
+	color: string;
+	icon: string;
 }
-const a = ConstantToMap<number, IValue, IValue["key"]>(
-	0,
-	{ text: "服务内" },
-	1,
-	{ text: "服务内1", color: "green1", key: "in" },
-	2,
-	{ text: "服务内2", color: "green2", key: "in" },
-	3,
-	{ text: "服务内3", color: "green3", key: 'in' },
-    4,{}
-).extend((args) => ({ ...args, default: -1 }));
+type c = AssetConstType["key"] | 'all';
+const ASSET_TYPE = ConstantToMap<AssetConstType>(
+	{ status: 0, key: "server", text: "终端", color: "red" },
+	{ status: 1, key: "terminal", text: "终端", color: "green" },
+	{ status: 2, key: "unknown", text: "终端", color: "green" }
+).extend({ default: 0 });
 
-const status = 1;
-console.log(a.keyMatch("in"));
+// 映射中文
+console.log("映射中文,ASSET_TYPE.matchStatus(status)", ASSET_TYPE.matchStatus(0));
+// 根据key 获取对应的字段
+console.log('根据key 获取对应的字段,ASSET_TYPE.matchKey("terminal")', ASSET_TYPE.matchKey("terminal"));
+// 默认值
+console.log("默认值,ASSET_TYPE.default", ASSET_TYPE.default);
+
+const a = ASSET_TYPE.toArray(([k, v]) => ({ value: k, label: v.name }));
+// list
+console.log("list,ASSET_TYPE.toArray(([k, v]) => ({ value: k, label: v.name }))", a.list[0].value);
 
 /**
+ *
  * 需求
  * 1. 根据 status 映射 text done
- * 2. 根据 key 匹配 status pending
- * 3. 转换成list
+ * 2. 根据 key 匹配 status done
+ * 3. 转换成list done
  */
 /**
  * 目标
+ * 1. 期望只传入value的类型即可 key的类型可以省略 done
  *
+ * 2. 能否结合 enum no
+ * 
+ * 3. 能否简单的实现全部值
  */

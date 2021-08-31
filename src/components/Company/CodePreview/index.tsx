d9ab318ab863dc18d6e4ba2618eda270d05b8156
clearink <1561939638@qq.com>
2021-09-01 00:02:02 +0800

    123

--- src/components/Company/CodePreview/index.tsx
+++ src/components/Company/CodePreview/index.tsx
@@ -1,21 +1,6 @@
-import { UnControlled as CodeMirror } from "react-codemirror2";
-import "codemirror/mode/cmake/cmake";
-import "codemirror/mode/xml/xml";
-import "codemirror/mode/javascript/javascript";
-import "codemirror/mode/clike/clike";
 import "./style.scss";
 
 // 代码预览
 export default function CodePreview(props: any) {
-	return (
-		<CodeMirror
-			value='<h1>I ♥ react-codemirror2</h1>'
-			options={{
-				mode: { name: "javascript", json: true },
-				theme: "material",
-				lineNumbers: true,
-			}}
-			onChange={(editor, data, value) => {}}
-		/>
-	);
+	return <div></div>;
 }

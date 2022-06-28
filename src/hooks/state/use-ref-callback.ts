0607ad1f439d95c63493795ac665b763a04e8b7b
clearink <1561939638@qq.com>
2022-06-28 20:21:32 +0800

    12321

--- src/hooks/state/use-ref-callback.ts
+++ src/hooks/state/use-ref-callback.ts
@@ -1,8 +1,8 @@
-import { useCallback, useRef } from "react";
+import { useCallback, useLayoutEffect, useRef } from "react";
 
 // 使用 ref 获得一个memoized 函数 该函数 引用不会变 但是永远会得到最新的数据
 export default function useRefCallback<T extends ArrowFunction>(callback: T): T {
-	const callbackRef = useRef(callback);
-	callbackRef.current = callback;
-	return useCallback((...args: any[]) => callbackRef.current(...args), []) as T;
+	const ref = useRef<T | null>(null);
+	useLayoutEffect(() => (ref.current = callback), [callback]);
+	return useCallback((...args: any[]) => ref.current?.(...args), []) as T;
 }
